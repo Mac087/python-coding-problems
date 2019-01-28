7259286e2b1e333b5e851682598f1847807ac12a
Andrew MacDonald <mac087@gmail.com>
2019-01-27 20:00:00 -0600

    Complete problem 49

--- Problem 49.py
+++ Problem 49.py
@@ -15,3 +15,15 @@
 #   b   c
 #  / \ / \
 # d  e f  g
+
+def reconstruct(preorder, inorder):
+    if not preorder and not inorder:
+        return None
+    if len(preorder) == len(inorder) == 1:
+        return preorder[0]
+
+    root = preorder[0]
+    root_i = inorder.index(root)
+    root.left = reconstruct(preorder[1:1 + root_i], inorder[0:root_i])
+    root.right = reconstruct(preorder[1 + root_i:], inorder[root_i + 1:])
+    return root
