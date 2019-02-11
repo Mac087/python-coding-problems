91b239458f09d3ed3874c5ed22aae64d708d5850
Andrew MacDonald <mac087@gmail.com>
2019-02-10 20:00:00 -0600

    Complete problem 56

--- Problem 56.py
+++ Problem 56.py
@@ -2,7 +2,7 @@
 
 # shorten(url), which shortens the url into a six-character alphanumeric string, such as zLg6wl.
 # restore(short), which expands the shortened string into the original url. If no such shortened string exists, return null.
-# Hint: What if we enter the same URL twice?
+# Hint: What if we enter the same URL twice? 
 
 import random
 import string
