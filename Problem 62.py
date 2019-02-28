637724cd249dce36254c2f7fc6c155f5fdbf1234
Andrew MacDonald <mac087@gmail.com>
2019-02-27 20:00:00 -0600

    Complete problem 62

--- Problem 62.py
+++ Problem 62.py
@@ -4,3 +4,19 @@
 
 # For example, pow(2, 10) should return 1024.
 
+def power(x, y):
+    base = x
+    exponent = y
+    if y < 0:
+        base = 1 / x
+        exponent = -y
+    coeff = 1
+    while y > 1:
+        if y % 2 == 0:
+            base *= base
+            y = y // 2
+        else:
+            coeff *= base
+            base *= base
+            y = (y - 1) // 2
+    return coeff * base
