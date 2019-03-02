70647f2dd08363b09456bd2a342135f5061997c2
Andrew MacDonald <mac087@gmail.com>
2019-03-01 20:00:00 -0600

    Complete problem 63

--- Problem 63.py
+++ Problem 63.py
@@ -5,3 +5,14 @@
 # Right, then down
 # Down, then right
 # Given a 5 by 5 matrix, there are 70 ways to get to the bottom-right.
+
+def num_ways(n, m):
+    A = [[0 for _ in range(m)] for _ in range(n)]
+    for i in range(n):
+        A[i][0] = 1
+    for j in range(m):
+        A[0][j] = 1
+    for i in range(1, n):
+        for j in range(1, m):
+            A[i][j] = A[i - 1][j] + A[i][j - 1]
+    return A[-1][-1]
