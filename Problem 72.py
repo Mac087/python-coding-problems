30f4290681facb67bba771b8fa11f451be2e3d12
Andrew MacDonald <mac087@gmail.com>
2019-03-18 20:00:00 -0600

    Complete problem 72

--- Problem 72.py
+++ Problem 72.py
@@ -1,3 +1,8 @@
 # Using a function rand7() that returns an integer from 1 to 7 (inclusive) with uniform probability, implement a function rand5() 
 # that returns an integer from 1 to 5 (inclusive).
 
+def rand5():
+    r = rand7()
+    if 1 <= r <= 5:
+        return r
+    return rand5()
