e2fee5902754375c4e4428019af2c4ef3a205a2c
Andrew MacDonald <mac087@gmail.com>
2019-03-22 20:00:00 -0600

    Complete problem 74

--- Problem 74.py
+++ Problem 74.py
@@ -1,2 +1,10 @@
 # Given the head of a singly linked list, reverse it in-place.
 
+def reverse(head):
+    prev, current = None, head
+    while current is not None:
+        tmp = current.next
+        current.next = prev
+        prev = current
+        current = tmp
+    return prev
