e4c55727dffd40adb1ba543672711ca19efe318b
Andrew MacDonald <mac087@gmail.com>
2019-03-30 20:00:00 -0600

    Complete problem 78

--- Problem 78.py
+++ Problem 78.py
@@ -4,3 +4,13 @@
 
 # For example, given [(1, 3), (5, 8), (4, 10), (20, 25)], you should return [(1, 3), (4, 10), (20, 25)].
 
+def merge(intervals):
+    result = []
+    for start, end in sorted(intervals, key=lambda i: i[0]):
+        # If current interval overlaps with the previous one, combine them
+        if result and start <= result[-1][1]:
+            prev_start, prev_end = result[-1]
+            result[-1] = (prev_start, max(end, prev_end))
+        else:
+            result.append((start, end))
+    return result
