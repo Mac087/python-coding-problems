8d3d229c6d650522eacd8405fd4f0a5cd88c990d
Andrew MacDonald <mac087@gmail.com>
2019-04-03 20:00:00 -0600

    Complete problem 80

--- Problem 80.py
+++ Problem 80.py
@@ -4,3 +4,13 @@
 
 # Given the array [10, 5, 1], you should return false, since we can't modify any one element to get a non-decreasing array.
 
+def check(lst):
+    count = 0
+    for i in range(len(lst) - 1):
+        if lst[i] > lst[i + 1]:
+            if count > 0:
+                return False
+            if i - 1 >= 0 and i + 2 < len(lst) and lst[i] > lst[i + 2] and lst[i + 1] < lst[i - 1]:
+                return False
+            count += 1
+    return True
