9ffbfaf72c989bea4c09fa655cc6e6ad66d1f706
Andrew MacDonald <mac087@gmail.com>
2019-04-11 20:00:00 -0600

    Complete problem 83

--- Problem 83.py
+++ Problem 83.py
@@ -2,3 +2,18 @@
 
 # For example, given a file with the content “Hello world”, three read7() returns “Hello w”, “orld” and then “”.
 
+class Reader:
+    def __init__(self):
+        self.remainder = ''
+
+    def readN(self, n):
+        result = self.remainder
+        text = None
+
+        while len(result) < n and (text is None or len(text) >= 5):
+            text = read7()
+            result += text
+
+        self.remainder = result[n:]
+
+        return result[:n]
